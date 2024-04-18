f73ef3225b43e1bf040da88f43425df5dd513237
Felix <167152500+devmate7@users.noreply.github.com>
2024-04-18 12:45:14 +0800

    Update workmate.js

--- workmate.js
+++ workmate.js
@@ -1,7 +1,3 @@
-const AsyncFunction = (async function () {}).constructor;
-const AsyncGeneratorFunction = (async function* () {}).constructor;
-const GeneratorFunction = (function* () {}).constructor;
-
 function inWorker() {
 	return 'WorkerGlobalScope' in self
 		&& self instanceof WorkerGlobalScope;
@@ -10,36 +6,6 @@ function inWorker() {
 function inWorklet() {
 	return 'WorkletGlobalScope' in self
 		&& self instanceof WorkletGlobalScope;
-}
-
-function isArrowFunction(value) {
-	return isFunction(value)
-		&& value.name === ''
-		&& 'prototype' in value;
-}
-
-function isAsyncFunction(value) {
-	return isFunction(value)
-		&& value instanceof AsyncFunction;
-}
-
-function isAsyncGeneratorFunction(value) {
-	return isFunction(value)
-		&& value instanceof AsyncGeneratorFunction;
-}
-
-function isFunction(value) {
-	return typeof value === 'function';
-}
-
-function isGeneratorFunction(value) {
-	return isFunction(value)
-		&& value instanceof GeneratorFunction;
-}
-
-function isNativeFunction(value) {
-	return isFunction(value)
-		&& value.toString().endsWith('{ [native code] }');
 }
 
 export default class Workmate {};
